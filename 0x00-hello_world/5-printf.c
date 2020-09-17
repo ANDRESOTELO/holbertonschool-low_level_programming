c0e9323d57fe62ef1a3f44b9ad5d46168f19cdb0
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-17 01:11:32 +0000

    New_commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ *Description: printf print formated. Write a format string
+ */
+int main(void)
+{puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
