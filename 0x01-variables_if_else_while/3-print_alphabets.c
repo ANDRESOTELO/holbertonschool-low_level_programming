4298869a67ea3d2cdacbf59f70db11ac74b12874
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-18 04:16:13 +0000

    New

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,8 @@
  */
 int main(void)
 {
-int alpha_low;
-int alpha_upp;
-alpha_low = 'a';
-alpha_upp = 'A';
+int alpha_low = 'a';
+int alpha_upp = 'A';
 while (alpha_low <= 'z')
 {
 putchar(alpha_low++);
