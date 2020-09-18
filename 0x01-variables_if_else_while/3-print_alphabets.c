d409d343ce3b6b9e973875559f6a82ec3c7d4d53
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-18 04:08:43 +0000

    New

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ *Description: puts used to write the characters by string.
+ */
+int main(void)
+{
+int alpha_low;
+int alpha_upp;
+alpha_low = 'a';
+alpha_upp = 'A';
+while (alpha_low <= 'z')
+{
+putchar(alpha_low++);
+}
+while (alpha_upp <= 'Z')
+{
+putchar(alpha_upp++);
+}
+putchar('\n');
+}
