dd20e71c9984eb17002da0d60c2ed29f590e4f34
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-22 16:01:53 +0000

    New commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,29 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ *print_alphabet - Entry point
  *
+ *print_alphabet_x10 - Entry point
  *
  */
-
 void print_alphabet(void)
 {
-  int alpha;
-  for  (alpha  = 'a'; alpha <= 'z'; alpha++)
-    {
-      _putchar(alpha);
-    }
+int alpha;
+for  (alpha  = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
 }
+}
+
+/**
+ *print_alphabet_x10  - Entry point
+ */
+
 void print_alphabet_x10(void)
 {
 int alpha;
- for (alpha = '0'; alpha <= '9'; alpha++)
-   {
- print_alphabet();
- _putchar('\n');
+for (alpha = '0'; alpha <= '9'; alpha++)
+{
+print_alphabet();
+_putchar('\n');
 }
 }
