30357fa51e476400b2b906d7d3130dc85b968f48
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-22 15:52:19 +0000

    New commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 /**
+ *print_alphabet - Entry point
  *
- *
+ *Return: Always 0 (Success)
  *
  */
 
 void print_alphabet(void)
 {
-  char alpha;
-  for  (alpha  = 'a'; alpha <= 'z'; alpha++)
-    {
-      _putchar(alpha);
-    }
-  _putchar('\n');
+char alpha;
+for  (alpha  = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
 }
