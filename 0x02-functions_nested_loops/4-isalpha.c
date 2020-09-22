9ca50e4f7a3696794fa3ea56f238c587c89a97ed
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-22 20:06:30 +0000

    commit _isalpha function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <ctype.h>
 /**
  *_isalpha -  checks alpha character neither lower or upper
  *
@@ -12,13 +11,11 @@ int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-isalpha(c);
 return (1);
 }
 else
 if (c >=  'A' && c <= 'Z')
 {
-isalpha(c);
 return (1);
 }
 else
