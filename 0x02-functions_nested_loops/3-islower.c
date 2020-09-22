dfdad895c0979c18910bc8f861b453c0f7a8c6cf
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-22 16:55:03 +0000

    New commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 #include<ctype.h>
 /**
- *_islower - checks a lowercase character
+ *_islower -  checks a lowercase character
  *
  *@c: character to check
  *
- *Return 1: Success
+ *Return: Success 1.
  */
 
 int _islower(int c)
 {
-if (c >= 'a'&& c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
 islower(c);
 return (1);
