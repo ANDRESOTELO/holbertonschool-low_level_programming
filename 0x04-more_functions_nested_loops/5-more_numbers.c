b46f74f3f01aeab9f2ad905a6987c6c726fda1e0
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-25 04:56:23 +0000

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,22 +9,21 @@
 
 void more_numbers(void)
 {
-  int numbers;
-  int lines;
-  for (lines = 0; lines <= 9; lines++)
-    {
-      for (numbers = 0; numbers <= 14; numbers++)
-	{
-	  if (numbers <= 9)
-	    {
-	      _putchar((numbers%10) + '0');
-	    }
-	  else
-	    {
-	      _putchar((numbers%10) + '0');
-	      _putchar((numbers%10) + '0');    
-	    }
-	}
-    }
-  _putchar('\n');
+int numbers;
+int lines;
+for (lines = 0; lines <= 9; lines++)
+{
+for (numbers = 0; numbers <= 14; numbers++)
+{
+if (numbers > 9)
+{
+_putchar((numbers / 10) + '0');
+}
+else
+{
+}
+_putchar((numbers % 10) + '0');
+}
+_putchar('\n');
+}
 }
