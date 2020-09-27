c78f20776e92a40da15dfd4c560a1d8f0abe69d4
ANDRESOTELO <andresotelo2020@gmail.com>
2020-09-27 01:13:40 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/.#7-print_diagonal.c
@@ -0,0 +1,1 @@
+root@vagrant-ubuntu-trusty-64.11263:1600467887
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,21 +3,27 @@
 /**
  *print_diagonal - prints diagonal line
  *
- *
+ *@n: dato que ingresa
  *
  *Return : Success
  */
 
 void print_diagonal(int n)
 {
-  if (n <= 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {
-      _putchar(92);
-    }
+int lines;
+int columns;
 
-
+if (n <= 0)
+{
+_putchar(10);
+}
+for (lines = 0; lines < n; lines++)
+{
+for (columns = 0; columns < lines; columns++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar(10);
+}
 }
