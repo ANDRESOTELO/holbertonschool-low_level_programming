efca9f223bbefd19aa584dd17fa0ea31cdd10d12
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-02 03:29:17 +0000

    New commit

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -23,3 +23,8 @@ char *_strncpy(char *dest, char *src, int n);
  *_strcmp - function that compares two strings.
  */
 int _strcmp(char *s1, char *s2);
+
+/**
+ *reverse_array - function that reverses the content of an array of integers.
+ */
+void reverse_array(int *a, int n);
