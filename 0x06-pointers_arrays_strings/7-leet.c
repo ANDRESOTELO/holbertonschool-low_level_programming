2f92f9cbb0879cb7cb3e8f773a90fbb302bcb6bc
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-03 05:00:04 +0000

    New Commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,27 @@
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
- *
+ * *leet - Entry point
+ * @s: pointer
+ * Return: Always 0 (Success)
  */
-
-char *leet(char *)
+char *leet(char *s)
 {
-char c1[] = 
+int cont, cont2;
 
+char n[] = "4433007711";
+char c[] = "aAeEoOtTlL";
+
+for (cont = 0 ; s[cont] != '\0' ; cont++)
+{
+for (cont2 = 0 ; cont2 <= 9 ; cont2++)
+{
+if (s[cont] == c[cont2])
+{
+s[cont] = n[cont2];
+}
+}
+}
+return (s);
 }
