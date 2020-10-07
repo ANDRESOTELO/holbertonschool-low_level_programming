e859939f748ac3e1ce8c672214a4c347857925d3
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-07 02:23:32 +0000

    New commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,19 +10,19 @@ char *_strpbrk(char *s, char *accept)
 {
 int i, j;
 
-for (i = 0 ; s[i] != '\0' ; s++)
-{
-for (j = 0 ; accept[j] != '\0' ; j++)
-{
-if (s[i] == accept[j])
-{
-return (s);
-}
-else if (s[i] == '\0')
-{
-return ('\0');
-}
-}
-}
-return ('\0');
+ for (i = 0 ; s[i] != '\0' ; s++)
+   {
+     for (j = 0 ; accept[j] != '\0' ; j++)
+       {
+	 if (s[i] == accept[j])
+	   {
+	     return (s);
+	   }
+	 else if (s[i] == '\0')
+	   {
+	     return ('\0');
+	   }
+       }
+   }
+ return ('\0');
 }
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -17,3 +17,8 @@ char *_strchr(char *s, char c);
  *function that gets the length of a prefix substring.
  */
 unsigned int _strspn(char *s, char *accept);
+
+/**
+ *function that locates a substring.
+ */
+char *_strstr(char *haystack, char *needle);
