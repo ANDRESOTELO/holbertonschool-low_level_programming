beb6a18a10aad7ea604d991f929802d36e1dd034
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-08 02:40:17 +0000

    factorial

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -17,3 +17,8 @@ void _print_rev_recursion(char *s);
  *_strlen_recursion - function that returns the length of a string.
  */
 int _strlen_recursion(char *s);
+
+/**
+ *function that returns the factorial of a given number.
+ */
+int factorial(int n);
