5c8472fc06d5022b7880acc65208d7e8a07c759d
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-08 03:25:27 +0000

    pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *_pow_recursion - Entry point
+ *
+ *@x: Number to raised
+ *@y: Numnber of raised
+ *
+ *Return: integer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+		if (y < 0)
+		{
+			return (-1);
+		}
+		else
+		{
+			return (x * _pow_recursion(x, y - 1));
+		}
+}
