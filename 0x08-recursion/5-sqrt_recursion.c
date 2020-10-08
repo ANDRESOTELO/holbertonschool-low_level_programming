6f40f8f99a7189d9b8846a387fa4e87469305882
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-08 05:02:09 +0000

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,30 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: number to get his natural square root.
+ * sqrt_check - checks for the square root of c
+ * @g:guess at sqrt
+ * @c: number to find sqrt of
  *
- * Return: -1.
+ * Return: -1 or sqrt of c
  */
-
-int _sqrt_recursion(int n)
+int sqrt_check(int g, int c)
 {
-	return (_sqrt(n, 0));
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_check(g + 1, c));
 }
 
-
 /**
- * _sqrt - calculates the natural square root of a number.
- * @n: number to get his natural square root
- * @i: number to see if it is the sqr root of n.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
  *
- * Return: -1.
+ * Return: natural square root or -1
  */
-
-int _sqrt(int n, int i)
+int _sqrt_recursion(int n)
 {
-	if (i * i > n)
-		return (-1);
-	else if (i * i == n)
-		return (i);
-	else
-		return (_sqrt(n, i + 1));
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
 }
