58d2f3867fa31bc814d89311a689e28eb1b3c1fe
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-11 04:14:23 +0000

    argc - print number of params

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: Argument count
+ *@argv: Argument value
+ *Return: Success Always 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+
+	printf("%d\n", argc);
+        return (0);
+}
