5bb2533e8ba3c772e4d1b2b253926e42b7c91d81
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-16 00:24:45 +0000

    Function that create array 2D

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *alloc_grid - function that returns a pointer to a 2D array of integers.
+ * @width: Columns
+ * @height: Rows
+ *
+ * Return: Double pointer - NULL if fails.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+
+	if ((width <= 0) || (height <= 0))
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(**array) * height);
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+
+		if (array[i] == NULL)
+		{
+
+			for (; i >= 0; --i)
+			{
+				free(array[i]);
+			}
+			free(array);
+			return (NULL);
+		}
+	}
+	return (array);
+}
