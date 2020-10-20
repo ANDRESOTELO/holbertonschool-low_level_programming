ab54e383e81da08ccd3880d89936a4617c350988
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-20 06:19:49 +0000

    function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *new_dog - function that creates a new dog.
+ *@name: Second member
+ *@age: Third member
+ *@owner: Fourth member
+ *Return: NULL if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *firulais;
+
+	firulais = malloc(sizeof(dog_t));
+
+	if (firulais == NULL)
+		return (NULL);
+
+	firulais->name = name;
+	firulais->age = age;
+	firulais->owner = owner;
+	return (firulais);
+}
