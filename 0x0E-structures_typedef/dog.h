fdea2793415017d12c12afacf36749df961a8594
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-20 05:35:20 +0000

    New prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,8 +18,17 @@ struct dog
 	char *owner;
 };
 
+/**
+ * Function that initialize a variable of type struct dog
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+
+/**
+ * Function that prints a struct dog
+ */
+
+void print_dog(struct dog *d);
 
 #endif /*_DOG_H_*/
