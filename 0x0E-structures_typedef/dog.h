981db3047ef387a0d4b3399b237acb3c565964a0
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-20 00:39:15 +0000

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Struct for dog and owner data
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Data about name, age and owner for a dog
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*_DOG_H_*/
