545acffdab727a68698343fcbd684031291ed2d4
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-21 22:45:01 +0000

    Function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - Function that prints a name.
+ *@name: name of the person
+ *@f: point to function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+	{
+		return;
+	}
+
+	f(name);
+}
