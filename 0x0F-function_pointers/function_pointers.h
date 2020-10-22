b15d418d15cc03469430a09787920f3475f7c3fc
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-22 01:19:20 +0000

    Prototypes of all functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -14,5 +14,7 @@ void print_name(char *name, void (*f)(char *));
 /*Func that executes a func given as parameter on each element of an array*/
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/*function that searches for an integer*/
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /*_FUNCTION_POINTERS_H_*/
