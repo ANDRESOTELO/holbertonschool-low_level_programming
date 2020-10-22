2c4b76505c2d33997f43fff9f0a04f4a7cf32ce2
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-22 00:05:43 +0000

    Prototypes of all functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,18 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /*Putchar prototype*/
 int _putchar(char c);
 
 /*Function that prints a name.*/
 
 void print_name(char *name, void (*f)(char *));
+
+/*Func that executes a func given as parameter on each element of an array*/
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 
 #endif /*_FUNCTION_POINTERS_H_*/
