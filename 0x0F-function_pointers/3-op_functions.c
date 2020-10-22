0a5b2d960fb11c561864bf3c85534facb5a1127b
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-22 19:45:38 +0000

    contain the operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+ *op_add - Funtion to add
+ *@a: Param Integer
+ *@b: Param Integer
+ *Return: Integer of sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - Funtion to substract
+ *@a: Param Integer
+ *@b: Param Integer
+ *Return: Integer of sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - Funtion to multiplies
+ *@a: Param Integer
+ *@b: Param Integer
+ *Return: Integer of mul
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - Funtion to div
+ *@a: Param Integer
+ *@b: Param Integer
+ *Return: Integer of div
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - Funtion to mod
+ *@a: Param Integer
+ *@b: Param Integer
+ *Return: Integer of mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
