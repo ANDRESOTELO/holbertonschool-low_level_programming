b684646cc06fad8aea94785c9190f1c28d840d57
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-26 02:23:14 +0000

    64-bit program in assembly that prints Hello, Holberton

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -2,7 +2,7 @@ section .data
 
 	message db "Hello, Holberton", 10
 
-	section .text
+section .text
 
 global main
 
@@ -11,8 +11,4 @@ main:
 	mov rdi, 1
 	mov rsi, message
 	mov rdx, 17
-	syscall
-
-	mov rax,60
-	mov rdx,0
 	syscall
