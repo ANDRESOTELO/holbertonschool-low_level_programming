765554253a18397cd675de93f1f99c663d6f3006
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-31 05:03:00 +0000

    V1.02 - Add comments

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 /*creo un puntero de tipo list_t llamado new_node*/
 	list_t *new_node;
-	unsigned int i;
+	unsigned int i;/*iterates the string*/
 
 /*defino la cantidad de memoria que necesito para almacenar un nodo*/
 /*y devuelvo un puntero de tipo list_t*/
@@ -23,10 +23,11 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
+/*i iterates the string and give us the num of characters*/
 	}
 	new_node->str = strdup(str);
 	new_node->len = i;
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = *head;/*new_node->next points to head*/
+	*head = new_node;/*now head values new_node*/
 	return (new_node);
 }
