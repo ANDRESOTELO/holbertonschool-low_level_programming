ef1e7653ce4f115ca2e80ead26493017e2565a97
ANDRESOTELO <andresotelo2020@gmail.com>
2020-10-31 04:59:42 +0000

    V1.0 - Add function that adds a new node at the beginning of the list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,22 +11,21 @@ list_t *add_node(list_t **head, const char *str)
 {
 /*creo un puntero de tipo list_t llamado new_node*/
 	list_t *new_node;
-	unsigned int i, len_str;
+	unsigned int i;
 
 /*defino la cantidad de memoria que necesito para almacenar un nodo*/
 /*y devuelvo un puntero de tipo list_t*/
-	new_node = (list_t *)malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		len_str = i + 1;
 	}
 	new_node->str = strdup(str);
-	new_node->len = len_str;
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
