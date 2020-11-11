d4479741a47a87d426605e01a97532e625505ff2
ANDRESOTELO <andresotelo2020@gmail.com>
2020-11-11 00:57:24 +0000

    V1.1 - Close correctly for loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,25 @@
 int create_file(const char *filename, char *text_content)
 {
 	int new_file, count;
-	char *wrt;
+	int wrt;
 
 	if (!filename)
 		return (-1);
 
 	/*open with the flag O_CREAT creates a file*/
-	new_file = open(filename, O_RDWR | O_CREAT | O_TRUNCT, 600);
+	new_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 
 	if (new_file == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		for (count = 0; wrt[count] != '\0'; count++)
+		for (count = 0; text_content[count] != '\0'; count++)
+		{
+		}
 			/*new_file is the file descriptor (3)*/
 			/*text_content is the text that writes in the new file*/
-			wrt = write(new_file, text_content, count);
+		wrt = write(new_file, text_content, count);
 		if (wrt == -1)
 			return (-1);
 	}
