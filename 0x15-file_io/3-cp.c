6d0eeb6b51327fcb4819339413f2eef517b11549
ANDRESOTELO <andresotelo2020@gmail.com>
2020-11-13 19:40:55 +0000

    V1.0 - Program that copies the content from a file into another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: arguments count
+ *@argv: arguments value
+ *Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, file_read, file_write;
+	char buffer[1024];
+
+	/*Can use comma(,) and end of line*/
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	if (argv[1] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
+	if (argv[2] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]),
+			exit(99);
+	/*create file_from and file_to. Both are int type*/
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
+	/*file_from is the file where read finds the characters*/
+	file_read = read(file_from, buffer, 1024);
+	while (file_read != 0)
+	{
+		file_write = write(file_to, buffer, file_read);
+		if (file_write == -1 || file_write != file_read)
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]),
+				exit(99);
+		file_read = read(file_from, buffer, 1024);
+		if (file_read == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+				exit(98);
+	}
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n'", file_to), exit(100);
+	return (0);
+}
