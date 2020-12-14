118d8bb6ebb2799c7a96d475937931e67f9a85f2
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-14 23:28:47 +0000

    Update - if temp->next = NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,8 +39,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->prev->next = new_node;
 			return (new_node);
 		}
+		if (tmp->next == NULL)
+			return (add_dnodeint_end(h, n));
 	}
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
 	return (NULL);
 }
