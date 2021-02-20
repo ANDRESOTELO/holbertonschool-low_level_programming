97a03bf4f73fd4a4008e70fa4d4202814a7b640a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-02-20 05:29:32 +0000

    Island perimeter pep8 and documentation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 """
 Island Perimeter
+
+Technical interview preparation:
+Create a function def island_perimeter(grid): that returns
+the perimete of the island described in grid:
+- grid is a list of list of integers:
+- 0 represents a water zone
+- 1 represents a land zone
+- One cell is a square with side length 1
+- Grid cells are connected horizontally/vertically (not diagonally).
+- Grid is rectangular, width and height dont exceed 100
+- Grid is completely surrounded by water, and there is one island
+(or nothing).
+- The island doesnt have lakes (water inside that isnt
+connected to the water around the island).
 """
 
 
