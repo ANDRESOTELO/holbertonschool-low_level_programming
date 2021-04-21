f3753b93be80b193d63dedd254fe8571c27c872a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-21 00:16:41 +0000

    UPDATED | Library

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 /*
  * Function that searches for a value in an array of integers
@@ -17,5 +18,12 @@ int linear_search(int *array, size_t size, int value);
  * using the Binary search algorithm
  */
 int binary_search(int *array, size_t size, int value);
+
+/*
+ * function that searches for a value in a sorted array of integers
+ * using the Jump search algorithm
+ */
+int jump_search(int *array, size_t size, int value);
+
 
 #endif /*_SEARCH_ALGOS_H_*/
